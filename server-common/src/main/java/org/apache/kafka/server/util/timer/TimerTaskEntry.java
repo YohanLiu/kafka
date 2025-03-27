37949f48445a6d69522c39295557c78a51c71a7a
yohan <yinhou.liu@huolala.cn>
2025-03-27 23:07:33 +0800

    延时操作模块源码-时间轮算法实现

--- server-common/src/main/java/org/apache/kafka/server/util/timer/TimerTaskEntry.java
+++ server-common/src/main/java/org/apache/kafka/server/util/timer/TimerTaskEntry.java
@@ -17,12 +17,20 @@
 package org.apache.kafka.server.util.timer;
 
 public final class TimerTaskEntry {
+    // 指定定时任务
     public final TimerTask timerTask;
+    // 定时任务的过期时间
     public final long expirationMs;
+    // 绑定的Bucket链表实例
+    // Kafka 的延时请求可能会被其他线程从一个链表搬移到另一个链表中，
+    // 因此，为了保证必要的内存可见性，代码声明 list 为 volatile
     volatile TimerTaskList list;
+    // next指针
     TimerTaskEntry next;
+    // prev指针
     TimerTaskEntry prev;
 
+    // 关联给定的定时任务
     public TimerTaskEntry(
         TimerTask timerTask,
         long expirationMs
@@ -37,10 +45,12 @@ public final class TimerTaskEntry {
         }
     }
 
+    // 关联定时任务是否已经被取消了
     public boolean cancelled() {
         return timerTask.getTimerTaskEntry() != this;
     }
 
+    // 从Bucket链表中移除自己
     public void remove() {
         TimerTaskList currentList = list;
         // If remove is called when another thread is moving the entry from a task entry list to another,
--- server-common/src/main/java/org/apache/kafka/server/util/timer/TimerTaskList.java
+++ server-common/src/main/java/org/apache/kafka/server/util/timer/TimerTaskList.java
@@ -26,7 +26,9 @@ import java.util.function.Consumer;
 
 class TimerTaskList implements Delayed {
     private final Time time;
+    // 标识当前这个链表中的总定时任务数
     private final AtomicInteger taskCounter;
+    // 表示这个链表所在 Bucket 的过期时间戳
     private final AtomicLong expiration;
 
     // TimerTaskList forms a doubly linked cyclic list using a dummy root entry
@@ -53,6 +55,9 @@ class TimerTaskList implements Delayed {
     }
 
     public boolean setExpiration(long expirationMs) {
+        // 随着时钟不断向前推进，原有 Bucket 会不断地过期，然后失效。
+        // 当这些 Bucket 失效后，源码会重用这些 Bucket。重用的方式就是重新设置 Bucket 的过期时间，并把它们加回到 DelayQueue 中。
+        // 这里进行比较的目的，就是用来判断这个 Bucket 是否要被插入到 DelayQueue
         return expiration.getAndSet(expirationMs) != expirationMs;
     }
 
@@ -69,12 +74,14 @@ class TimerTaskList implements Delayed {
         }
     }
 
+    // 将给定定时任务插入到链表
     public void add(TimerTaskEntry timerTaskEntry) {
         boolean done = false;
         while (!done) {
             // Remove the timer task entry if it is already in any other list
             // We do this outside of the sync block below to avoid deadlocking.
             // We may retry until timerTaskEntry.list becomes null.
+            // 在添加之前尝试移除该定时任务，保证该任务没有在其他链表中
             timerTaskEntry.remove();
 
             synchronized (this) {
@@ -85,6 +92,7 @@ class TimerTaskList implements Delayed {
                         timerTaskEntry.next = root;
                         timerTaskEntry.prev = tail;
                         timerTaskEntry.list = this;
+                        // 把timerTaskEntry添加到链表末尾
                         tail.next = timerTaskEntry;
                         root.prev = timerTaskEntry;
                         taskCounter.incrementAndGet();
@@ -95,6 +103,7 @@ class TimerTaskList implements Delayed {
         }
     }
 
+    // 从链表中移除定时任务
     public synchronized void remove(TimerTaskEntry timerTaskEntry) {
         synchronized (timerTaskEntry) {
             if (timerTaskEntry.list == this) {
@@ -108,13 +117,21 @@ class TimerTaskList implements Delayed {
         }
     }
 
+    // flush 方法是清空链表中的所有元素，并对每个元素执行指定的逻辑。
+    // 该方法用于将高层次时间轮 Bucket 上的定时任务重新插入回低层次的 Bucket 中
     public synchronized void flush(Consumer<TimerTaskEntry> f) {
+        // 找到链表第一个元素
+        // 第一个节点是哨兵节点，所以它的下一个元素才是真正的第一个元素
         TimerTaskEntry head = root.next;
+        // 开始遍历链表
         while (head != root) {
+            // 移除遍历到的链表元素
             remove(head);
+            // 执行传入参数f的逻辑
             f.accept(head);
             head = root.next;
         }
+        // 清空过期时间设置
         expiration.set(-1L);
     }
 
